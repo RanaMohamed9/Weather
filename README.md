f137471ca870271e75e768c39f89637bdad9ecef
Rana <rana.eltamamy@gmail.com>
2020-12-17 13:18:00 +0200

    add files

---
+++ README.md
@@ -0,0 +1,12 @@
+# Weather-Journal App Project
+This project requires you to create an asynchronous web app that uses Web API and user data to dynamically update the UI. 
+## Node
+In this project we use Node and Express environemnts that should be installed first. The project file server.js should require express(), and should create an instance of their app using express.
+The Express app instance should be pointed to the project folder with .html, .css, and .js files.
+## Dependencies
+cors package should be installed and using an instance from it.
+body-parser should be installed.
+## Local server
+Local server should be running and producing feedback to the Command Line through a working callback function.
+##API
+Create API credentials on OpenWeatherMap.com.
---
+++ website/app.js
@@ -0,0 +1,64 @@
+/* Global Variables */
+const apiKey = '&units=e2cdfdfc897e6e6d5a2501a2a1ae4b61';
+const apiURL = 'api.openweathermap.org/data/2.5/weather?'
+const Datee = document.getElementById('date');
+const Temp = document.getElementById('temp');
+const Content = document.getElementById('content');
+const PostCode = document.getElementById('zip');
+const Feelings = document.getElementById('feelings');
+const postURL = 'http://localhost:3000/adddata'
+const getURL = 'http://localhost:3000/all'
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+document.getElementById('generate').addEventListener('click', Action);
+function Action(e)  {
+  getTemperature(apiURL,PostCode.value,apiKey)
+  .then( (data) => {
+    postData(postURL,data = {temp: data.main.temp, date: newDate, content : feelings.value })
+  }).then(function() {
+    UpdateUI();
+  })    
+}
+
+const getTemperature = async (apiURL , PostCode , api)=>{
+    const response = await fetch(`http://${apiURL}zip=${PostCode}&appid=${api}`);
+    try{
+        const data = await response.json();
+        return data;
+    }
+    catch(error){
+        console.log('This is an error message!' , error);
+    }
+}
+
+const postData = async ( url , data = {})=>{
+    console.log(data)
+      const postresponse = await fetch(url, {
+      method: 'POST', 
+      credentials: 'same-origin', 
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data), 
+    });
+  
+      try {
+        const newData = await postresponse.json();
+        return newData
+      }catch(error) {
+      console.log('This is an error message!', error);
+      }
+  }
+
+const UpdateUI = async()=>{
+    const request = await fetch(getURL);
+    try {
+        const alldata = await request.json();
+        Datee.innerHTML = alldata.date;
+        Temp.innerHTML = alldata.temp;
+        Content.innerHTML = alldata.content;
+    }
+    catch(error){
+        console.log('This is an error message!' , error);
+    }}
